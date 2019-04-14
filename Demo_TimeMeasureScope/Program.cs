451c4169645665da95726a46c20d5f60d74bda92
freeman6 <freeman6@gmail.com>
2019-04-14 20:13:48 +0800

    加入專案檔案。

---
+++ Demo_TimeMeasureScope/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSF.AOP;
+using CSF.Common;
+
+namespace Demo_TimeMeasureScope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = ProxyFactory.Create<Calculator>();
+
+            var result1 = obj.Addition(11, 11);
+            var result2 = obj.Subtraction(10, 11);
+            var result3 = obj.Multiplication(10, 11);
+            var result4 = obj.Division(10, 1);
+            Console.WriteLine($"加法:{result1}\n減法:{result2}\n乘法:{result3}\n除法:{result4}");
+            Console.ReadLine();
+        }
+    }
+    public class Calculator : MarshalByRefObject
+    {
+        //加法
+        [TimeMeasure]
+        public int Addition(int x, int y) => x + y;
+        //減法
+        [TimeMeasure]
+        public int Subtraction(int x, int y) => x - y;
+        //乘法
+        [TimeMeasure]
+        public int Multiplication(int x, int y) => x * y;
+        //除法
+        [TimeMeasure]
+        public int Division(int x, int y) => x / y;
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Demo 如何使用 ExecutionHistory  Component
+
+### TDC 標準 .NET執行軌跡記錄器元件
+***
+### Installation Instructions
+#### Nuget Client:
+<code>nuget install CSF.Component.ExecutionHistory -Version 1.0.1 -Source http://nuget.csf.org.tw/nuget/internal/</code>
+
+#### Package Manager Console:
+<code>Install-Package CSF.Component.ExecutionHistory -Version 1.0.1 -Source http://nuget.csf.org.tw/nuget/internal//</code>
+
+#### 重要說明
+###### 請先於組態檔中加入下述參數設定
+`<appSettings>
+    <add key="IsDebugMode" value="true"/>
+</appSettings>`
